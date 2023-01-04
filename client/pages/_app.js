db0a30560e545837344b91c3068eba860aae30d0
saradehi <sarahidms@gmail.com>
2023-01-04 20:38:18 -0300

    deploy

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -4,7 +4,7 @@ import { wrapper } from "../src/store";
 import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "js-mongo-app-production.up.railway.app";
 
 const MyApp = ({ Component, session, ...rest }) => {
   const { store, props } = wrapper.useWrappedStore(rest);
