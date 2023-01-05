0ff950636dcdcbb68187e06963d866b4e63576b4
saradehi <sarahidms@gmail.com>
2023-01-04 22:11:48 -0300

    .env

--- client/pages/api/auth/[...nextauth].js
+++ client/pages/api/auth/[...nextauth].js
@@ -3,11 +3,15 @@ import GoogleProvider from 'next-auth/providers/google'
 
 
 export default NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        }) 
-    ],
-    secret: process.env.JWT_SECRET
-})
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.JWT_SECRET,
+});
