f4d0d820756dcbc45eacacb6f78f0e704645b9d2
saradehi <sarahidms@gmail.com>
2023-01-04 21:08:38 -0300

    deploy

--- client/pages/profile.js
+++ client/pages/profile.js
@@ -26,13 +26,7 @@ export default profile
 
 export const getServerSideProps = async (context) => {
     const session = await getSession(context);
-    if(!session) {
-        return {
-            redirect: {
-                destination: '/login'
-            }
-        }
-    };
+   
 
     return {
         props: { session }
