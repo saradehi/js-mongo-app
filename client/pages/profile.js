9cc926740533e64b29176fc647da4cdafacb359f
saradehi <sarahidms@gmail.com>
2023-01-04 21:13:20 -0300

    deploy

--- client/pages/profile.js
+++ client/pages/profile.js
@@ -1,25 +1,34 @@
 import React from "react";
 import { useSession, signOut } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 
 const profile = () => {
     const {data: session} = useSession()
 
     return (
-        <div>
-            {
-                session ? 
-                <div>
-                    <Image src={session.user.image} width={150} height={150} style={{borderRadius: '50%'}} alt={session.user.name}></Image>
-                    <h1>Welcome, {session.user.name}</h1>
-                    <button onClick={() => signOut()}>Sign out</button>
-                </div> : 
-                <div>
-                    <p>You are not sign in</p>
-                    <button onClick={()=> signIn()}>Sign in</button>
-                </div>
-            }
-        </div>
+      <div>
+        {session ? (
+          <div>
+            <Image
+              src={session.user.image}
+              width={150}
+              height={150}
+              style={{ borderRadius: "50%" }}
+              alt={session.user.name}
+            ></Image>
+            <h1>Welcome, {session.user.name}</h1>
+            <button onClick={() => signOut()}>Sign out</button>
+          </div>
+        ) : (
+          <div>
+            <p>You are not sign in</p>
+            <Link href={'/login'}>
+              <button type="button">Go back</button>
+            </Link>
+          </div>
+        )}
+      </div>
     );
 };
 
