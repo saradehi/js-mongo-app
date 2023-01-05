54332865549281de99aaaf1b71b373f2ec539ca6
saradehi <sarahidms@gmail.com>
2023-01-04 21:14:22 -0300

    deploy

--- client/pages/login.js
+++ client/pages/login.js
@@ -7,28 +7,38 @@ export default function () {
     console.log(session)
     
     return (
-        <>
-            {
-                session ? 
-                <div>
-                    <p>Go to <Link href={'/profile'}>{session.user.name}'s</Link> profile.</p>
-                </div> 
-                : <div>
-                    <h1>LOG IN</h1>
-                    <div>
-                        <form>
-                            <label>User: <input type='text'></input></label>
-                            <br />
-                            <label>Password: <input type='text'></input></label>
-                        </form>
-                        <button type="submit">Log in</button>
-                    </div>
-                    <div>
-                        <h1>SIGN IN</h1>
-                        <button onClick={() => signIn()}>Sign in</button>
-                    </div>
-                </div> 
-            }
-        </>
-    )
+      <>
+        {session ? (
+          <div>
+            <p>
+              Go to{" "}
+              <Link href={"/profile"}>
+                <button type="button">{session.user.name}'s</button>
+              </Link>{" "}
+              profile.
+            </p>
+          </div>
+        ) : (
+          <div>
+            <h1>LOG IN</h1>
+            <div>
+              <form>
+                <label>
+                  User: <input type="text"></input>
+                </label>
+                <br />
+                <label>
+                  Password: <input type="text"></input>
+                </label>
+              </form>
+              <button type="submit">Log in</button>
+            </div>
+            <div>
+              <h1>SIGN IN</h1>
+              <button onClick={() => signIn()}>Sign in</button>
+            </div>
+          </div>
+        )}
+      </>
+    );
 }
