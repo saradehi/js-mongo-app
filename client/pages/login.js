c2be354cc387392f825c0085f9eb9359a89a1a82
saradehi <sarahidms@gmail.com>
2023-01-10 18:39:07 -0300

    image

--- client/pages/login.js
+++ client/pages/login.js
@@ -68,12 +68,10 @@ export default function () {
         <title>Login</title>
       </Head>
       <section className="w-3/4 mx-auto flex flex-col gap-10">
-        <div>
+        <div className="title">
           <Link href={"/"}>
             <a>Go back Home</a>
           </Link>
-        </div>
-        <div className="title">
           <h1 className="text-gay-800 text-4xl font-bold py-4">Log in</h1>
           <p className="w-3/4 mx-auto text-gray-400">
             Login with your user and password
